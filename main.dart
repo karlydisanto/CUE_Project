f140d0885d6fadba2f8e7ea9e724eb616d0a6897
Karly DiSanto <70341133+karlydisanto@users.noreply.github.com>
2021-05-18 11:05:06 -0400

    Add files via upload

---
+++ main.dart
@@ -0,0 +1,42 @@
+import 'package:fit4u/locator.dart';
+import 'package:fit4u/models/exercise.dart';
+import 'package:fit4u/services/auth.dart';
+import 'package:fit4u/services/exercise_database.dart';
+import 'package:fit4u/wrapper.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:provider/provider.dart';
+import 'package:fit4u/models/user.dart';
+
+void main()async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  await setupLocator(); // setting up authservice object
+  runApp(MyApp());
+}
+class MyApp extends StatelessWidget {
+
+  // getting from locator
+  AuthService _auth = locator.get<AuthService>();
+
+  @override
+  Widget build(BuildContext context) {
+
+    return MultiProvider(
+      providers: [
+        StreamProvider<TheUser>.value(value: _auth.user),
+        StreamProvider<Exercise>.value(value: DatabaseService().exerciseData)
+      ],
+      child: MaterialApp(
+        home: Wrapper(),
+      ),
+    );
+      /*
+      StreamProvider<TheUser>.value(
+      value: _auth.user,
+
+    );*/
+  }
+}
---
+++ wrapper.dart
@@ -0,0 +1,22 @@
+import 'package:fit4u/screens/authenticate.dart';
+import 'package:fit4u/screens/home.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:fit4u/models/user.dart';
+import 'package:fit4u/models/exercise.dart';
+
+class Wrapper extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    //accessing user data and if signed out return null
+    final user = Provider.of<TheUser>(context);
+
+    //return either home or authenticate widget
+    if(user == null) {
+      return Authenticate();
+    }
+    else {
+      return Home();
+    }
+  }
+}
